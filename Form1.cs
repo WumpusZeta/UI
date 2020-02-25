67f220b61afd820273bb34a5dfee077a5005772d
dhruv-naik <57720551+dhruv-naik@users.noreply.github.com>
2020-02-24 20:05:51 -0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    class DisplayUI
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void DisplayPlayer(/*gameLocation, playerobject*/)
+        {
+            //displays the player on the screen in the given location
+        }
+        public int DisplayHS(/*HighScore, Score*/)
+        {
+            //when tab is hit a popup apear on screen displaying the current score and highScore
+            int HS = 1000;
+            return HS;
+        }
+        public static void DisplayTrivia(/*TriviaQs*/)
+        {
+            //Displays the trivia questions
+        }
+        public static void DisplayWumpus(/*Wumpus*/)
+        {
+            //When player encounters wumpus the wumpous is displayed on screen.
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String HS = "1000";
+            HighScoreDisplay.Text = HS;
+        }
+        private System.Windows.Forms.Button button1;
+
+
+        private System.Windows.Forms.Label HighScoreDisplay;
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+            int HS = 1000;
+        }
+    }
+
+}
---
+++ Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class Form2 : Form
+    {
+
+        DisplayUI myui;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myui = new DisplayUI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = myui.DisplayHS().ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
